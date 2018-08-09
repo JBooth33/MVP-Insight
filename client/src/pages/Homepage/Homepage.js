1aa407eefa8403a741b1c43c3755c3d3455ab7c0
BunnygotFangs <kimberly@thehilts.com>
2018-08-08 20:34:34 -0500

    adding footer component

--- client/src/pages/Homepage/Homepage.js
+++ client/src/pages/Homepage/Homepage.js
@@ -179,26 +179,6 @@ const Card = props => (
               </div>
             </div>
   
-         // Footer
-            <footer id="footer">
-              <div className="inner">
-    
-                <section>
-                  <h2>Follow</h2>
-                  <ul className="icons">
-                    <li><a href="#" className="icon style2 fa-facebook"><span className="label">Facebook</span></a></li>
-                    <li><a href="#" className="icon style2 fa-instagram"><span className="label">Instagram</span></a></li>
-                    <li><a href="#" className="icon style2 fa-linkedin"><span className="label">linkedIn</span></a></li>
-                    <li><a href="#" className="icon style2 fa-phone"><span className="label">Phone</span></a></li>
-                    <li><a href="#" className="icon style2 fa-envelope-o"><span className="label">Email</span></a></li>
-                  </ul>
-                </section>
-                <ul className="copyright">
-                  <li>&copy; Untitled. All rights reserved 2018</li>
-                </ul>
-              </div>
-            </footer>
-  
         </div>
   
   
